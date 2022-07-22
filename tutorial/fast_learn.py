f6ebc914bf0e2a0a4f381c697e9ff7973f1180e6
macbook pro <ahmad.fight@gmail.com>
2022-07-22 14:23:58 +0700

    python introduction

--- tutorial/fast_learn.py
+++ tutorial/fast_learn.py
@@ -1,3 +1,6 @@
+from operator import truediv
+
+
 fruits = ["apple","banana", "cherry", "kiwi", "manggo"]
 newlist = []
 
@@ -5,3 +8,33 @@ for x in fruits:
     if "a" in x:
         newlist.append(x)
 print(newlist)
+
+
+buah = ["apel", "pisang", "kiwi", "jeruk", "mangga"]
+lisBaru = [x for x in fruits if "a" in x]
+
+print(lisBaru)
+
+buah1 = ["apple", "manggo", "cherry", "banana", "kiwi"]
+buah1.sort()
+print(buah1)
+
+number = [100, 78, 36, 52, 10, 45]
+number.sort()
+print(number)
+
+#descending
+buah2 = ["apple", "manggo", "cherry", "banana", "kiwi"]
+buah2.sort(reverse = True)
+print(buah2)
+
+#descending
+number1 = [100, 78, 36, 52, 10, 45]
+number1.sort(reverse= True)
+print(number1)
+
+# if else
+a = 5
+b = 6
+if a % 5 and b % 6:
+    print("KAS")
