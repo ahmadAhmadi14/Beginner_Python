33c91ccda0b95ed662a9241ad965f19c495d8578
macbook pro <ahmad.fight@gmail.com>
2022-07-22 20:00:52 +0700

    list and dictionary

---
+++ tutorial/dictionary.py
@@ -0,0 +1,16 @@
+# list array
+data_list = [5, 7, 4, 16]
+print(data_list)
+print(data_list[0])
+
+# with {} -> set. 
+data_set = {5, 2, 90, 51, 87}
+print(data_set) # ascending
+# print(data_set[0]) --> error 'set' object is not subscriptable
+
+#tuples
+tuples = (6, 7, 18, 10, 3)
+print(tuples)
+print(tuples[0])
+
+# tuples[0] = 57 --> TypeError: tuple object does not support in item assignment
